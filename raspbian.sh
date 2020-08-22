548f10e48426d5d600c8dbb5309bfde4891e89dd
Katin <katin.imes@gmail.com>
2020-08-22 10:03:01 -0700

    Update raspbian.sh
    add screen blanking cancellation

--- raspbian.sh
+++ raspbian.sh
@@ -5,10 +5,18 @@ sudo apt update
 
 # install some favorites
 sudo apt install vim -y
-sudo apt install nmap
+sudo apt install nmap -y
+
 
 
 # security: remove flash
 sudo rm /usr/lib/chromium-browser/libpepflashplayer.so
 sudo rm /usr/lib/chromium-browser/flash_manifest.json
 sudo rm /usr/lib/chromium-browser/Flash.htm
+
+# stop screen blanking
+echo "xset s 0" >> /home/pi/.profile
+echo "xset -dpms" >> /home/pi/.profile
+
+# and reboot for some settings to take effect
+init 6
